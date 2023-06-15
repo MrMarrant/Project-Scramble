f21ce7d0670a802c6eb07dca0c9078b7b67a198d
MrMarant <laughymclaugher@gmail.com>
2023-06-15 18:45:06 +0200

    [UPDATE] default model in code

--- lua/scramble/config/sh_scramble_config.lua
+++ lua/scramble/config/sh_scramble_config.lua
@@ -6,29 +6,30 @@ if not file.Exists("data_scramble", "DATA") then
 end
 
 if not file.Exists(SCRAMBLE_CONFIG.PathConfigFile, "DATA") then
-    local SERVER_VALUES = {}
-    SERVER_VALUES["models/scp096anim/player/scp096pm_raf.mdl"] = {
-        head = "ValveBiped.Bip01_Head1", -- BoneId
-        x = 0, -- Pos Vector x adjustement depend on the model
-        y = 1, -- Pos Vector y adjustement depend on the model
-        z = 3  -- Pos Vector z adjustement depend on the model
-    }
-    SERVER_VALUES["models/shaklin/scp/096/scp_096.mdl"] = {
-        head = "head",
-        x = 0,
-        y = 2,
-        z = 0
-    }
-    SERVER_VALUES["models/player/scp096.mdl"] = {
-        head = "ValveBiped.Bip01_Head1",
-        x = 0,
-        y = 0,
-        z = 0
-    }
-    file.Write(SCRAMBLE_CONFIG.PathConfigFile, util.TableToJSON(SERVER_VALUES, true))
+    file.Write(SCRAMBLE_CONFIG.PathConfigFile, util.TableToJSON( {}, true ))
 end
 
 SCRAMBLE_CONFIG.ModelName = scramble.GetDataFromFile(SCRAMBLE_CONFIG.PathConfigFile)
+
+-- Default Model manage
+SCRAMBLE_CONFIG.ModelName["models/scp096anim/player/scp096pm_raf.mdl"] = {
+    head = "ValveBiped.Bip01_Head1", -- BoneId
+    x = 0, -- Pos Vector x adjustement depend on the model
+    y = 1, -- Pos Vector y adjustement depend on the model
+    z = 3  -- Pos Vector z adjustement depend on the model
+}
+SCRAMBLE_CONFIG.ModelName["models/shaklin/scp/096/scp_096.mdl"] = {
+    head = "head",
+    x = 0,
+    y = 2,
+    z = 0
+}
+SCRAMBLE_CONFIG.ModelName["models/player/scp096.mdl"] = {
+    head = "ValveBiped.Bip01_Head1",
+    x = 0,
+    y = 0,
+    z = 0
+}
 
 scramble.LoadDirectory(SCRAMBLE_CONFIG.RootFolder.."server/")
 scramble.LoadDirectory(SCRAMBLE_CONFIG.RootFolder.."client/")
