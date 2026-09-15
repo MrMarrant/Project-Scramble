93dde2983926a822f1279dcc19321ca1aaf020af
MrMarrant <laughymclaugher@gmail.com>
2026-09-15 19:49:11 +0200

    [FEATURE] Add Fetch system to get all models detected

--- lua/scramble/config/sh_scramble_config.lua
+++ lua/scramble/config/sh_scramble_config.lua
@@ -12,80 +12,21 @@ end
 
 SCRAMBLE_CONFIG.ModelName = scramble.GetDataFromFile(SCRAMBLE_CONFIG.PathConfigFile)
 
--- Default Model manage
-SCRAMBLE_CONFIG.ModelName["models/scp096anim/player/scp096pm_raf.mdl"] = {
-    head = "ValveBiped.Bip01_Head1", -- BoneId
-    x = 0, -- Pos Vector x adjustement depend on the model
-    y = 1, -- Pos Vector y adjustement depend on the model
-    z = 3  -- Pos Vector z adjustement depend on the model
-}
-SCRAMBLE_CONFIG.ModelName["models/shaklin/scp/096/scp_096.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 2,
-    z = 0
-}
-SCRAMBLE_CONFIG.ModelName["models/player/scp096.mdl"] = {
-    head = "ValveBiped.Bip01_Head1",
-    x = 0,
-    y = 0,
-    z = 0
-}
-SCRAMBLE_CONFIG.ModelName["models/scp_096/scp_096_final.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 0,
-    z = 1
-}
-SCRAMBLE_CONFIG.ModelName["models/cpthazama/scp/096.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 2.5,
-    z = 0,
-    scale = 0.7
-}
-SCRAMBLE_CONFIG.ModelName["models/cpthazama/scp/096_old.mdl"] = {
-    head = "Bone_077",
-    x = 0,
-    y = 0,
-    z = 3,
-    scale = 0.7
-}
-SCRAMBLE_CONFIG.ModelName["models/scpsl/dughoo/dughoo_scp096.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 2,
-    z = 3,
-    scale = 0.8
-}
-SCRAMBLE_CONFIG.ModelName["models/elan/scpsl/096/096.mdl"] = {
-    head = "Head",
-    x = 8,
-    y = 0,
-    z = 0,
-    scale = 0.7
-}
-SCRAMBLE_CONFIG.ModelName["models/painkiller_76/sf2/new096/096_v2.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 0,
-    z = 0,
-    scale = 0.9
-}
-SCRAMBLE_CONFIG.ModelName["models/painkiller_76/sf2/new096/096b.mdl"] = {
-    head = "head",
-    x = 0,
-    y = 0,
-    z = 0,
-    scale = 0.9
-}
-SCRAMBLE_CONFIG.ModelName["models/washton/scp096/scp096unity.mdl"] = {
-    head = "Jaw",
-    x = 0,
-    y = 0,
-    z = 0,
-    scale = 0.5
-}
+SCRAMBLE_CONFIG.PastebinModelURL = "https://pastebin.com/raw/AWupxWnw"
+
+http.Fetch(SCRAMBLE_CONFIG.PastebinModelURL, function(body)
+    local data = util.JSONToTable(body)
+    if not data then
+        print("[SCRAMBLE Arctic] Invalid JSON received from pastebin model config.")
+        return
+    end
+
+    for model, params in pairs(data) do
+        SCRAMBLE_CONFIG.ModelName[model] = params
+    end
+end, function(err)
+    print("[SCRAMBLE Arctic] Failed to fetch model config : "..err)
+end)
 
 scramble.LoadDirectory(SCRAMBLE_CONFIG.RootFolder .. "server/")
 scramble.LoadDirectory(SCRAMBLE_CONFIG.RootFolder .. "client/")
